be22d6afee9142e9d1f35b2e0a50c368ecb240fa
Tomaz <tmedrado99@gmail.com>
2020-05-11 17:19:09 -0300

    mailer testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  after_create :welcome_send
+
   has_many :participations
   has_many :events, :through => :participations
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+  
 end
